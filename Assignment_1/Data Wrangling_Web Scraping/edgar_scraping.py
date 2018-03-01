45c4933c1821dc66c29171c5763c626305e0d13b
deosthale.s <deosthale.s@husky.neu.edu>
2018-03-01 17:38:52 -0500

    Assignment 1 Added

--- Assignment_1/Data Wrangling_Web Scraping/edgar_scraping.py
+++ Assignment_1/Data Wrangling_Web Scraping/edgar_scraping.py
@@ -21,7 +21,14 @@ CIK = str(sys.argv[1])
 acc_no = str(sys.argv[2]) 
 AWS_ACCESS_KEY_ID = str(sys.argv[3])
 AWS_SECRET_ACCESS_KEY = str(sys.argv[4])
-#inputLocation = input("Bucket Location = ")
+inputLocation = str(sys.argv[5])
+
+
+'''CIK = input("CIK = ") 
+acc_no = input("Accession_no = ")
+AWS_ACCESS_KEY_ID = input("AWS_ACCESS_KEY_ID = ")
+AWS_SECRET_ACCESS_KEY = input("AWS_SECRET_ACCESS_KEY = ")
+inputLocation = input("Bucket Location = ")'''
 
 ## Creating the Loggers
 logger = logging.getLogger("Problem1")
@@ -36,28 +43,46 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
 
-## Generating the URL
+if not AWS_ACCESS_KEY_ID or not AWS_SECRET_ACCESS_KEY:
+    logger.warning('Access Key and Secret Access Key not provided')
+    print('Access Key and Secret Access Key not provided')
+    exit()
 
-baseURL = "https://www.sec.gov"
-if(str(CIK) != "" and str(acc_no) != ""):
-	CIK = CIK.lstrip('0')
-	def insert_dash(string):
-	    if (len(string) == 18):
-	        return string[:10] + "-" + string[10:12] + "-" + string[12:]
 
-else:
-	logger.warning("Invalid CIK or Accession_no")
+try:
+	#Creating a S3 bucket
+	conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+	logger.info("Connected to S3")
+	print("Connected to S3")
+except:
+	logger.info("Amazon keys are invalid")
+	print("Amazon keys are invalid")
 	exit()
 
-    
+
+if not CIK or not acc_no:
+    logger.warning('CIK or AccessionNumber was not given, assuming it to be 0000051143 and 0000051143-13-000007 respectively')
+    CIK='0000051143'
+    acc_no = '0000051143-13-000007'
+else:
+    logger.info('CIK: %s and AccessionNumber: %s given', CIK, acc_no)
+
+## Generating the URL
+baseURL = "https://www.sec.gov"
 	
+CIK = CIK.lstrip('0')
+def insert_dash(string):
+    if (len(string) == 18):
+        return string[:10] + "-" + string[10:12] + "-" + string[12:]
+
+
 accession_no = insert_dash(acc_no)
 
 def create_url(baseURL,CIK,acc_no,accession_no):
 	return str(baseURL + "/Archives/edgar/data/"+ CIK+"/" + acc_no + "/"+ accession_no + "-index.html")
 logger.info("Valid CIK and Accession_no provided")
 
-logger.info("-index.html URL created")
+logger.info("%s URL created", baseURL)
 
 URL = create_url(baseURL,CIK,acc_no,accession_no)
 print(URL)
@@ -65,20 +90,16 @@ externalList = []
 tablelist = []
 
 def create_10q_url():
- 	try:
-		soup = BeautifulSoup((urllib.request.urlopen(URL)),"html.parser")
-		for t in soup.find_all('table' , attrs={"summary": "Document Format Files"}):
-		     for tr in soup.find_all('tr'):
-		        for td in tr.findChildren('td'):
-		            if(td.text == '10-Q'):
-		                for a in tr.findChildren('a', href=True):
-		                    return str(a['href'])
+	soup = BeautifulSoup((urllib.request.urlopen(URL)),"html.parser")
+	for t in soup.find_all('table' , attrs={"summary": "Document Format Files"}):
+	     for tr in soup.find_all('tr'):
+	        for td in tr.findChildren('td'):
+	            if(td.text == '10-Q'):
+	                for a in tr.findChildren('a', href=True):
+	                    return str(a['href'])
 
-
-	logger.info("URL for the 10-q file for CIK = %s and Accession_no = %s is created", CIK,acc_no)
-    except:
-    	logger.warning("URL for the 10-q file for CIK = %s and Accession_no = %s is not found", CIK,acc_no)
-
+logger.info("URL for the 10-q file for CIK = %s and Accession_no = %s is created", CIK,acc_no)
+            
                   
 #print(finalURL)
 
@@ -152,11 +173,9 @@ zipping('/', zf, tablelist)
 zf.close()
 logger.info("CSV files and the Log file were zipped in a single folder")
 
-#Creating a S3 bucket
-conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 
 #Location for the region
-'''loc = ''
+loc = ''
 if inputLocation == 'APNortheast':
     loc=boto.s3.connection.Location.APNortheast
 elif inputLocation == 'APSoutheast':
@@ -174,29 +193,30 @@ elif inputLocation == 'SAEast':
 elif inputLocation == 'USWest':
     loc=boto.s3.connection.Location.USWest
 elif inputLocation == 'USWest2':
-    loc=boto.s3.connection.Location.USWest2'''
+    loc=boto.s3.connection.Location.USWest2
+
 
 bucket_name = ts = time.time()
 st = datetime.datetime.fromtimestamp(ts)    
-bucket_name =  "statistical data " + str(CIK) + str(st).replace(" ", "").replace("-", "").replace(":","").replace(".","")
-bucket = conn.create_bucket(bucket_name, location=boto.s3.connection.Location.DEFAULT)
+bucket_name = AWS_ACCESS_KEY_ID.lower()+str(st).replace(" ", "").replace("-", "").replace(":","").replace(".","")
+bucket = conn.create_bucket(bucket_name, location=loc)
 filename = ('%s.zip' %CIK)
 logger.info("S3 bucket successfully created")
 
-
 #Uploading files to the Bucket
-def percent_cb(complete, total):
-	sys.stdout.write('.')
-	sys.stdout.flush()
+try:
+	def percent_cb(complete, total):
+		sys.stdout.write('.')
+		sys.stdout.flush()
 
-k = Key(bucket)
-k.key = 'Problem1'
-k.set_contents_from_filename(filename, cb=percent_cb, num_cb=10)
+	k = Key(bucket)
+	k.key = 'Problem1'
+	k.set_contents_from_filename(filename, cb=percent_cb, num_cb=10)
+except:
+	logger.warning("Invalid Amazon Keys.")
+	print("Invalid Amazon Keys.")
+	exit()
 
-'''except:
-    logger.warning("Invalid Amazon Keys.")
-    print("Invalid Amazon Keys.")
-    exit()'''
 logger.info("Zip file successfully uploaded to S3")
 print("Zip File successfully uploaded to S3")
 
